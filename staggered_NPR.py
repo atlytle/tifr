e157a184de025c157a1c5b02254f7ae7ab11a257
Andrew Lytle <atlytle+github@gmail.com>
2013-04-02 10:58:55 +0530

    staggered_NPR: ~500x speedup in polespace matrices. Need to check correctness, document, clean up.

--- staggered_NPR.py
+++ staggered_NPR.py
@@ -71,10 +71,10 @@ def hypercubic(S, F, A, B):
     '''Hypercubic matrix elements.'''
     return (1./4)*trace(reduce(dot, [hc(G[A]), G[S], G[B], hc(G[F])]))
     
-hypercubic_cache = np.array([[[[hypercubic(S,F,A,B) for B in range(16)]
-                                                    for A in range(16)] 
-                                                    for F in range(16)]
-                                                    for S in range(16)])
+#hypercubic_cache = np.array([[[[hypercubic(S,F,A,B) for B in range(16)]
+#                                                    for A in range(16)] 
+#                                                    for F in range(16)]
+#                                                    for S in range(16)])
 
 def hypercubicM(S, F):
     '''Hypercubic staggered matrices.'''
@@ -84,23 +84,70 @@ def hypercubicM(S, F):
 def dotp(A,B):
     return np.dot(dtob4(A), dtob4(B))
     
+dotpM = np.array([[np.dot(dtob4(A), dtob4(B)) for B in range(16)]
+                                              for A in range(16)])
+    
+def sfun(A, B, C, D):
+    return (-1)**(dotp(A,C) + dotp(D,B))
+    
+def sfunM(A,B):
+    tmp = np.array([[dotp(A,C) + dotp(D,B) for D in range(16)]
+                                           for C in range(16)]) # fromfunction?
+    return np.cos(np.pi*tmp)
+    
+def sfunM2(A,B):
+    tmp = dotpM[A,:] + dotpM[:,B].reshape((16,1))
+    return np.cos(np.pi*tmp)
+    
 def polespace(S, F, A, B):
     result = 0.
+    H = hypercubicM(S,F)
+    #Sfun = sfunM(A,B)
     for C in range(16):
         for D in range(16):
             sign = (-1)**(dotp(A,C) + dotp(D,B))  # Can speed up.
-            result += sign*hypercubic_cache[S,F,C,D]
+            result += sign*H[C,D]
     return (1./16)*result
     
-polespace_cache = np.array([[[[polespace(S,F,A,B) for B in range(16)]
-                                                    for A in range(16)] 
-                                                    for F in range(16)]
-                                                    for S in range(16)])
+def polespace2(S, F, A, B):
+    H = hypercubicM(S,F)
+    Sfun = sfunM(A,B)
+    return (1./16)*np.sum(H*Sfun)
+    
+def polespace3(S, F):
+    H = hypercubicM(S,F)
+    tmp = np.array([[np.sum(H*sfunM2(A,B)) for B in range(16)]
+                                          for A in range(16)])
+    return (1./16)*tmp
+    
+#polespace_cache = np.array([[[[polespace(S,F,A,B) for B in range(16)]
+#                                                    for A in range(16)] 
+#                                                    for F in range(16)]
+#                                                    for S in range(16)])
     
 def polespaceM(S,F):
     '''Polespace matrices.'''
     return np.array([[polespace_cache[S,F,A,B] for B in range(16)] 
                       for A in range(16)])
+                      
+def main():
+#    print 'calculating hypercubicM 1 1'
+#    hc = hypercubicM(1,1)
+#    print 'calculating SfunM 1 1'
+#    sf = sfunM(1,1)
+    print 'calculating polespace 1 1'
+    ps = np.array([[polespace(6,11,A,B) for B in range(16)] for A in range(16)])
+#    print 'calculating polespace2 1 1'
+#    ps2 = np.array([[polespace2(1,1,A,B) for B in range(16)] for A in range(16)])
+    print 'calculating polespace3 1 1'
+    ps3 = polespace3(6,11)
+     
+    print ps.all() == ps3.all()
+    #import cProfile
+    #cProfile.run('polespace3(1,1)')
+    
+if __name__ == "__main__":
+    main()
     
         
     
