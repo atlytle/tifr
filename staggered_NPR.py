130e99430369fa7e2cc3d30707044187f588e91a
Andrew Lytle <atlytle+github@gmail.com>
2013-04-02 12:23:39 +0530

    staggered_NPR.py: Clean-up.

--- staggered_NPR.py
+++ staggered_NPR.py
@@ -28,7 +28,7 @@ gamma[3] = np.array([[0, 0, 1, 0],
 id4 = np.identity(4, dtype=complex)
 
 
-# QDP enumeration.
+# QDP enumeration of gamma matrices.
 
 def hc(M):
     "Hermitian conjugate."
@@ -58,6 +58,7 @@ def one_or_M(M, num):
         raise TypeError("M must be a 4x4 array")
 
 def Gamma(n):
+    '''QDP enumeration of gamma matrices.'''
     binary = dtob4(n)
     gammas = [gamma[0], gamma[1], gamma[2], gamma[3]]
     gammas = map(one_or_M, gammas, binary)  # gamma[0]^n[0]..gamma[3]^n[3]
@@ -65,89 +66,70 @@ def Gamma(n):
 
 G = [Gamma(n) for n in range(16)]
 
-# Staggered matrices.
+# Hypercubic matrices.
 
 def hypercubic(S, F, A, B):
     '''Hypercubic matrix elements.'''
     return (1./4)*trace(reduce(dot, [hc(G[A]), G[S], G[B], hc(G[F])]))
-    
-#hypercubic_cache = np.array([[[[hypercubic(S,F,A,B) for B in range(16)]
-#                                                    for A in range(16)] 
-#                                                    for F in range(16)]
-#                                                    for S in range(16)])
 
 def hypercubicM(S, F):
     '''Hypercubic staggered matrices.'''
     return np.array([[hypercubic(S,F,A,B) for B in range(16)] 
                       for A in range(16)])
+
+# Helper functions.
            
 def dotp(A,B):
     return np.dot(dtob4(A), dtob4(B))
     
-dotpM = np.array([[np.dot(dtob4(A), dtob4(B)) for B in range(16)]
-                                              for A in range(16)])
-    
 def sfun(A, B, C, D):
     return (-1)**(dotp(A,C) + dotp(D,B))
     
-def sfunM(A,B):
-    tmp = np.array([[dotp(A,C) + dotp(D,B) for D in range(16)]
-                                           for C in range(16)]) # fromfunction?
-    return np.cos(np.pi*tmp)
+dotpM = np.array([[np.dot(dtob4(A), dtob4(B)) for B in range(16)]  
+                                              for A in range(16)])
     
-def sfunM2(A,B):
+def sfunM(A,B):
+    '''Fast implementation making use of cached dotpM;
+    S(A,B)_{CD} == (-1)**(dotp(A,C) + dotp(D,B))'''
+    
     tmp = dotpM[A,:] + dotpM[:,B].reshape((16,1))
     return np.cos(np.pi*tmp)
     
+# Polespace matrices.
+    
 def polespace(S, F, A, B):
+    '''Naive implementation of polespace matrix elements.'''
     result = 0.
     H = hypercubicM(S,F)
-    #Sfun = sfunM(A,B)
     for C in range(16):
         for D in range(16):
-            sign = (-1)**(dotp(A,C) + dotp(D,B))  # Can speed up.
+            sign = sfun(A, B, C, D)  # Can speed up.
             result += sign*H[C,D]
     return (1./16)*result
+ 
+def polespaceM(S, F):
+    '''Fast implementation of polespace matrices;
     
-def polespace2(S, F, A, B):
-    H = hypercubicM(S,F)
-    Sfun = sfunM(A,B)
-    return (1./16)*np.sum(H*Sfun)
+    P(S,F)_{A,B} == H(S,F)_{CD} S(A,B)_{CD}.'''
     
-def polespace3(S, F):
     H = hypercubicM(S,F)
-    tmp = np.array([[np.sum(H*sfunM2(A,B)) for B in range(16)]
+    tmp = np.array([[np.sum(H*sfunM(A,B)) for B in range(16)]
                                           for A in range(16)])
     return (1./16)*tmp
-    
-#polespace_cache = np.array([[[[polespace(S,F,A,B) for B in range(16)]
-#                                                    for A in range(16)] 
-#                                                    for F in range(16)]
-#                                                    for S in range(16)])
-    
-def polespaceM(S,F):
-    '''Polespace matrices.'''
-    return np.array([[polespace_cache[S,F,A,B] for B in range(16)] 
-                      for A in range(16)])
                       
-def main():
-#    print 'calculating hypercubicM 1 1'
-#    hc = hypercubicM(1,1)
-#    print 'calculating SfunM 1 1'
-#    sf = sfunM(1,1)
-    print 'calculating polespace 1 1'
+def test():
+    print 'calculating polespace naive'
     ps = np.array([[polespace(6,11,A,B) for B in range(16)] for A in range(16)])
-#    print 'calculating polespace2 1 1'
-#    ps2 = np.array([[polespace2(1,1,A,B) for B in range(16)] for A in range(16)])
-    print 'calculating polespace3 1 1'
-    ps3 = polespace3(6,11)
-     
+    print 'calculating polespace fast'
+    ps3 = polespaceM(6,11)
+    print 'Same result? ',
     print ps.all() == ps3.all()
-    #import cProfile
-    #cProfile.run('polespace3(1,1)')
+    
+    import cProfile
+    cProfile.run('polespaceM(1,1)')
     
 if __name__ == "__main__":
-    main()
+    test()
     
         
     
