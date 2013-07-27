962813ce96752ec830649f5080aa71c00ecdbf9a
Andrew Lytle <atlytle@gmail.com>
2013-07-27 13:59:15 +0530

    plots for LATT2013.

--- LATT2013_plots.py
+++ LATT2013_plots.py
@@ -51,33 +51,32 @@ def plot_fDs(results, save=False, saveas='', title=None, legend_spec='',
 def main(argv=None):
     options = parse_args(argv)
     root = '/Users/atlytle/Dropbox/TIFR/figs/'
+        
+    # L32T96.
+    print '---------- L32T96 ------------'
+    ainv = 2222.15  # (20) MeV.
+    csq = 0.85  # c^2.
+    charm_masses = [0.425, 0.430, 0.435]
+    strange_masses = [0.046, 0.048, 0.049]
     
-    print '------------- L48T144 -------------'
-    
-    ainv = 3390.48  # (23) MeV
-    csq = 0.93  # c^2.
-    charm_masses = [0.28, 0.29, 0.3]
-    strange_masses = [0.027, 0.028, 0.029, 0.030]
-    
-    # mc = 0.29
+    # mc = 0.430
     fDs = []  # Aggregate results.
-    for mc, ms in [(0.29, 0.027), (0.29, 0.028), (0.29, 0.029), (0.29, 0.03)]:
-        print '\n__mc = {0}, ms = {1}__'.format(mc, ms)
-        x = Overlap_T144(mc, ms)
-        spec = 'L48T144_c{0}_s{1}'.format(mc, ms)
+    for mc, ms in [(0.430, 0.046), (.430, .048), (0.430, 0.049)]:
+        print '__mc = {0}, ms = {1}__'.format(mc, ms)
+        x = Overlap_T96(mc, ms)
+        spec = 'L32T96_c{0}_s{1}'.format(mc, ms)
         
         print 'Point-Point pseudoscalar:'
         name = root + spec + '_pscalar_pp_'.format(mc, ms)
         if options.plot:
             plot_correlator(x.pscalar_pp, options.save, name+'corr.pdf', spec)
             plot_effmass(x.pscalar_pp, options.save, name+'meff_naive.pdf', 
-                         spec, [0.4,0.7])
+                         spec, [0.6,1.0])
         p1, err, chisq = fit_twopoint_cfuns(x.pscalar_pp.real, 20, 42, x.T)
         print 'A=', p1[0], '+/-', err[0]
         print 'm=', p1[1], '+/-', err[1], '-->', p1[1]*ainv/csq, '+/-',\
                                                  err[1]*ainv/csq
         print 'chisq=', chisq
-        
         # f_Ds.
         JK = fit_twopoint_cfunsJK(x.pscalar_pp.real, 20, 42, x.T)
         A_JK, M_JK = np.array(zip(*JK))
@@ -88,28 +87,27 @@ def main(argv=None):
         fDs.append((ms, ave*ainv, sig*ainv))
     plot_fDs(fDs, save=True, 
              saveas='/Users/atlytle/Desktop/fDs_c{0}.pdf'.format(mc), 
-             title='$am_c={0}$, $1/a = 3390$ MeV'.format(mc), xlabel='am_s',
-             xlim=[.026,.031], ylim=[210,270]) 
-             
-    # ms = 0.028         
-    fDs = []  # Aggregate results.       
-    for mc, ms in [(0.28, 0.028), (0.29, 0.028), (0.3, 0.028)]:
-        print '\n__mc = {0}, ms = {1}__'.format(mc, ms)
-        x = Overlap_T144(mc, ms)
-        spec = 'L48T144_c{0}_s{1}'.format(mc, ms)
+             title='$am_c={0}$, $1/a = 2222$ MeV'.format(mc), xlabel='am_s',
+             xlim=[.045,.050], ylim=[])
+
+    # ms = 0.048
+    fDs = []  # Aggregate results.
+    for mc, ms in [(0.425, 0.048), (.430, .048), (0.435, 0.048)]:
+        print '__mc = {0}, ms = {1}__'.format(mc, ms)
+        x = Overlap_T96(mc, ms)
+        spec = 'L32T96_c{0}_s{1}'.format(mc, ms)
         
         print 'Point-Point pseudoscalar:'
         name = root + spec + '_pscalar_pp_'.format(mc, ms)
         if options.plot:
             plot_correlator(x.pscalar_pp, options.save, name+'corr.pdf', spec)
             plot_effmass(x.pscalar_pp, options.save, name+'meff_naive.pdf', 
-                         spec, [0.4,0.7])
+                         spec, [0.6,1.0])
         p1, err, chisq = fit_twopoint_cfuns(x.pscalar_pp.real, 20, 42, x.T)
         print 'A=', p1[0], '+/-', err[0]
         print 'm=', p1[1], '+/-', err[1], '-->', p1[1]*ainv/csq, '+/-',\
                                                  err[1]*ainv/csq
         print 'chisq=', chisq
-        
         # f_Ds.
         JK = fit_twopoint_cfunsJK(x.pscalar_pp.real, 20, 42, x.T)
         A_JK, M_JK = np.array(zip(*JK))
@@ -120,8 +118,80 @@ def main(argv=None):
         fDs.append((mc, ave*ainv, sig*ainv))
     plot_fDs(fDs, save=True, 
              saveas='/Users/atlytle/Desktop/fDs_s{0}.pdf'.format(ms), 
-             title='$am_s={0}$, $1/a = 3390$ MeV'.format(ms), xlabel='am_c',
-             xlim=[.275,.305], ylim=[225,260])
+             title='$am_s={0}$, $1/a = 2222$ MeV'.format(ms), xlabel='am_c',
+             xlim=[], ylim=[225,260])
+    
+    # L48T144.
+#    print '------------- L48T144 -------------'
+#    
+#    ainv = 3390.48  # (23) MeV
+#    csq = 0.93  # c^2.
+#    charm_masses = [0.28, 0.29, 0.3]
+#    strange_masses = [0.027, 0.028, 0.029, 0.030]
+#    
+#    # mc = 0.29
+#    fDs = []  # Aggregate results.
+#    for mc, ms in [(0.29, 0.027), (0.29, 0.028), (0.29, 0.029), (0.29, 0.03)]:
+#        print '\n__mc = {0}, ms = {1}__'.format(mc, ms)
+#        x = Overlap_T144(mc, ms)
+#        spec = 'L48T144_c{0}_s{1}'.format(mc, ms)
+#        
+#        print 'Point-Point pseudoscalar:'
+#        name = root + spec + '_pscalar_pp_'.format(mc, ms)
+#        if options.plot:
+#            plot_correlator(x.pscalar_pp, options.save, name+'corr.pdf', spec)
+#            plot_effmass(x.pscalar_pp, options.save, name+'meff_naive.pdf', 
+#                         spec, [0.4,0.7])
+#        p1, err, chisq = fit_twopoint_cfuns(x.pscalar_pp.real, 20, 42, x.T)
+#        print 'A=', p1[0], '+/-', err[0]
+#        print 'm=', p1[1], '+/-', err[1], '-->', p1[1]*ainv/csq, '+/-',\
+#                                                 err[1]*ainv/csq
+#        print 'chisq=', chisq
+#        
+#        # f_Ds.
+#        JK = fit_twopoint_cfunsJK(x.pscalar_pp.real, 20, 42, x.T)
+#        A_JK, M_JK = np.array(zip(*JK))
+#        tmpJK = f_P(mc, ms, M_JK, A_JK)
+#        ave, sig = tmpJK[0], JKsigma(tmpJK)
+#        print 'f_Ds = ', ave, '+/-', sig, '-->',\
+#                         ave*ainv, '+/-', sig*ainv,  '\n'
+#        fDs.append((ms, ave*ainv, sig*ainv))
+#    plot_fDs(fDs, save=False, 
+#             saveas='/Users/atlytle/Desktop/fDs_c{0}.pdf'.format(mc), 
+#             title='$am_c={0}$, $1/a = 3390$ MeV'.format(mc), xlabel='am_s',
+#             xlim=[.026,.031], ylim=[210,270]) 
+#             
+#    # ms = 0.028         
+#    fDs = []  # Aggregate results.       
+#    for mc, ms in [(0.28, 0.028), (0.29, 0.028), (0.3, 0.028)]:
+#        print '\n__mc = {0}, ms = {1}__'.format(mc, ms)
+#        x = Overlap_T144(mc, ms)
+#        spec = 'L48T144_c{0}_s{1}'.format(mc, ms)
+#        
+#        print 'Point-Point pseudoscalar:'
+#        name = root + spec + '_pscalar_pp_'.format(mc, ms)
+#        if options.plot:
+#            plot_correlator(x.pscalar_pp, options.save, name+'corr.pdf', spec)
+#            plot_effmass(x.pscalar_pp, options.save, name+'meff_naive.pdf', 
+#                         spec, [0.4,0.7])
+#        p1, err, chisq = fit_twopoint_cfuns(x.pscalar_pp.real, 20, 42, x.T)
+#        print 'A=', p1[0], '+/-', err[0]
+#        print 'm=', p1[1], '+/-', err[1], '-->', p1[1]*ainv/csq, '+/-',\
+#                                                 err[1]*ainv/csq
+#        print 'chisq=', chisq
+#        
+#        # f_Ds.
+#        JK = fit_twopoint_cfunsJK(x.pscalar_pp.real, 20, 42, x.T)
+#        A_JK, M_JK = np.array(zip(*JK))
+#        tmpJK = f_P(mc, ms, M_JK, A_JK)
+#        ave, sig = tmpJK[0], JKsigma(tmpJK)
+#        print 'f_Ds = ', ave, '+/-', sig, '-->',\
+#                         ave*ainv, '+/-', sig*ainv,  '\n'
+#        fDs.append((mc, ave*ainv, sig*ainv))
+#    plot_fDs(fDs, save=False, 
+#             saveas='/Users/atlytle/Desktop/fDs_s{0}.pdf'.format(ms), 
+#             title='$am_s={0}$, $1/a = 3390$ MeV'.format(ms), xlabel='am_c',
+#             xlim=[.275,.305], ylim=[225,260])
     
     return 0
     
