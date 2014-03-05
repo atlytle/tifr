964d6b3f8608b07542b8bce7041be486ae22485b
Andrew Lytle <atlytle@gmail.com>
2014-03-05 18:15:43 +0530

    Encapsulate propagator read.

--- read_HISQ.py
+++ read_HISQ.py
@@ -4,6 +4,7 @@ import numpy as np
 
 nx, ny, nz, nt = 24, 24, 24, 64
 nc = 3
+nfloat = nx*ny*nz*nt*nc*nc*2  # Number of 4byte numbers expected.
                 
 def extract_t(clist, t):
 # c c t z y x
@@ -29,33 +30,37 @@ def extract_t3(clist, t):
     result = clist[t*nz*ny*nx*nc*2:t*nz*ny*nx*nc*2 + nz*ny*nx*nc*2]
     assert len(result) == nc*nz*ny*nx*2
     return result
-        
+
+def pion_correlator(filename):
+    correlator = np.zeros((nt))
+    f = open(filename, 'rb')
+    
+    # Read header.  This consists of 22 4byte values.
+    data = np.fromfile(f, dtype='<i', count=5)  # 5 floats.
+    timestamp = np.fromfile(f, dtype='<c', count=64)  # 16 floats.
+    order = np.fromfile(f, dtype='<i', count=1)  # 1 float.
+    
+    # Read data and calculate pion correlator.
+    # Each chunk of data is separated by an int specifying source color,
+    # and two other 4byte values.  Thus there are 31 'extra' 4bytes.
+    result = np.zeros((nt))
+    for c in range(nc):
+        color = np.fromfile(f, dtype='<i', count=1)
+        some_nums = np.fromfile(f, dtype='<f', count=2) 
+        x = np.fromfile(f, dtype='<f', count=nfloat/3)
+        # Add intermediate results to correlator.
+        # The sum is done in double precision.
+        for t in range(nt):
+            correlator[t] += np.square(extract_t3(x, t)).astype(np.float64).sum()
+    
+    f.close()
+    return correlator
+            
                 
 def main(filename):
-    nfloat = nx*ny*nz*nt*nc*nc*2  # Number of 4byte numbers expected.
-
-    with open(filename, "rb") as f:
-        # Read header.  This consists of 22 4byte values.
-        data = np.fromfile(f, dtype='<i', count=5)  # 5 floats.
-        timestamp = np.fromfile(f, dtype='<c', count=64)  # 16 floats.
-        order = np.fromfile(f, dtype='<i', count=1)  # 1 float.
-        print data
-        print timestamp
-        print order
-        # Read data and calculate pion correlator.
-        # Each chunk of data is separated by an int specifying source color,
-        # and two other 4byte values.  Thus there are 31 'extra' 4bytes.
-        result = np.zeros((nt))
-        for c in range(nc):
-            print 'color', np.fromfile(f, dtype='<i', count=1)
-            print 'some nums', np.fromfile(f, dtype='<f', count=2) 
-            x = np.fromfile(f, dtype='<f', count=nfloat/3)
-            # Add intermediate results to correlator.
-            # The sum is done in double precision.
-            for t in range(nt):
-                result[t] += np.square(extract_t3(x, t)).astype(np.float64).sum()
-        for t in range(nt):
-            print t, result[t]
+    correlator = pion_correlator(filename)
+    for t in range(nt):
+        print t, correlator[t]
          
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1]))
