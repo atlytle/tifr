c163ba580591fe979ef60774ec28573f6358d716
Andrew Lytle <atlytle@gmail.com>
2014-03-05 16:04:41 +0530

    Encapsulate parse routine.

--- read_overlap.py
+++ read_overlap.py
@@ -3,50 +3,38 @@ import struct
 import itertools
 import numpy as np
 
-nx, ny, nz, nt = 32, 32, 32, 96
+nx, ny, nz, nt = 24, 24, 24, 64
 V = nx*ny*nz  # Spatial volume.
 nc = 3  # N_c.
 ns = 4  # N_spin.
 ndouble = V*nt*nc*nc*ns*ns*2  # Number of 8byte numbers expected.
 
-def extract_t(data, t):
-    block = V*nc*nc*ns*ns*2
-    return data[t*block:t*block + block]  
+def prop_name(config, m):
+    return 'l2464f211b600m0102m0509m635a.'\
+    '{0}.gf_hyp_prop_src_wall_t00_m{1:7f}'.format(config, m)
 
-def reorder(data):
-    "Reorder the data in a sensible way."
-    num = -1
-    assert len(data) == ndouble
-    result = np.zeros((ndouble))
-    loop = itertools.product(range(nt), range(ns), range(nc), range(2), 
-                             range(ns), range(nc), range(nz), range(ny), 
-                             range(nx))
-    for t, s2, c2, ri, s1, c1, z, y, x in loop:
-        num += 1
-        old = x + nx*y + nx*ny*z + nx*ny*nz*c1 + nx*ny*nz*nc*s1 + \
-              nx*ny*nz*nc*ns*ri + nx*ny*nz*nc*ns*2*c2 + \
-              nx*ny*nz*nc*ns*2*nc*s2 + nx*ny*nz*nc*ns*2*nc*ns*t
-        assert old == num
-        new = ri + 2*c2 + 2*nc*s2 + 2*nc*ns*c1 + 2*nc*ns*nc*s1 + \
-              2*nc*ns*nc*ns*x + 2*nc*ns*nc*ns*nx*y + 2*nc*ns*nc*ns*nx*ny*z +\
-              2*nc*ns*nc*ns*nx*ny*nz*t
-        result[new] = data[old]
-    assert num == ndouble - 1
-    assert 0 not in result
-    return result
+def correlator_name(m):
+    return 'OOpion_l2464__m{0:7f}_m{0:7f}.npy'
 
-                
-def main(filename):
+def extract_t(filename, t):
+    '''Extract the pion correlation function at time t from the propagator.'''
+    tmp = []
+    for i in range(2*nc*nc*ns*ns):
+        with open(filename, "rb") as f:  # Inefficient.
+            f.seek(i*8*nt*V + 8*t*V, 0)
+            data = np.fromfile(f, dtype='>d', count=V)
+            tmp.append(np.sum(np.square(data)))
+    return np.sum(tmp)
+
+def pion_correlator(filename):
+    correlator = np.zeros((nt))
     for t in range(nt):
-        result = []
-        for i in range(2*nc*nc*ns*ns):
-            with open(filename, "rb") as f:
-                f.seek(i*8*nt*V+8*t*V, 0)
-                data = np.fromfile(f, dtype='>d', count=V)
-                result.append(np.sum(np.square(data)))
-            
-        print t, sum(result)
-    
+        correlator[t] = extract_t(filename, t)
+    return correlator
+
+
+def main(filename):
+   np.save(correlator_name(0.005), pion_correlator(filename)) 
          
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1]))
