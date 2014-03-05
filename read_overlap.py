adbd8292d2155d632dd816d94a3332416b7bc59a
Andrew Lytle <atlytle@gmail.com>
2014-03-05 17:24:41 +0530

    Construct pions from overlap props (not tested).

--- read_overlap.py
+++ read_overlap.py
@@ -3,6 +3,8 @@ import struct
 import itertools
 import numpy as np
 
+from os.path import getsize
+
 nx, ny, nz, nt = 24, 24, 24, 64
 V = nx*ny*nz  # Spatial volume.
 nc = 3  # N_c.
@@ -14,7 +16,7 @@ def prop_name(config, m):
     '{0}.gf_hyp_prop_src_wall_t00_m{1:7f}'.format(config, m)
 
 def correlator_name(m):
-    return 'OOpion_l2464__m{0:7f}_m{0:7f}.npy'
+    return 'OOpion_l2464__m{0:7f}_m{0:7f}.npy'.format(m)
 
 def extract_t(filename, t):
     '''Extract the pion correlation function at time t from the propagator.'''
@@ -32,10 +34,39 @@ def pion_correlator(filename):
         correlator[t] = extract_t(filename, t)
     return correlator
 
+def check_length(file):
+    '''Ensure the file has the expected size.'''
+    if getsize(file) != ndouble*8:
+        raise Exception('{0} does not have the expected size.'.format(file))
 
-def main(filename):
-   np.save(correlator_name(0.005), pion_correlator(filename)) 
+
+def main(files):
+    # Some basic checks on the input.
+    head0, config0, middle0, mass0 = files[0].split('.')
+    for f in files:
+        check_length(f)
+        head, config, middle, mass = f.split('.')
+        if (head != head0) or (middle != middle0) or (mass != mass0):
+            print "You might not want to combine these!"
+            return 1
+
+    # Construct the block of correlators.
+    correlators = []
+    for f in files:
+        correlators.append(pion_correlator(f))
+    correlators = np.array(correlators)
+
+    # Basic checks on the output.
+    print correlators.shape
+    assert (len(files), nt) == correlators.shape
+
+    # Write output.
+    m = float('0.'+mass0)
+    np.save(correlator_name(m), correlators) 
+    
+    return 0
+   
          
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1]))
+    sys.exit(main(sys.argv[1:]))
 
