9a8247159d2f3f2f98c9250c0d115d4f9b1f5427
Andrew Lytle <atlytle@gmail.com>
2014-03-07 10:20:36 +0530

    Incremental change.

--- overlap_pion.py
+++ overlap_pion.py
@@ -5,18 +5,19 @@ from resample import yield_JK_sample_np, JK_block, block
 from calc_overlap import plot_correlator, plot_effmass, fit_twopoint_cfuns
 
 root = ''
-mlist = [0.0745]
+mlist = [0.0745, 0.001] #0.001 has no signal
 
 cs = [np.load(correlator_name(m)) for m in mlist]
 csJK = map(JK_block, cs)
 print cs[0].shape
 print csJK[0].shape
 t=0
-for x in csJK[0][0]:  # correlator
+for x in cs[0][0]:  # correlator
     print t, x
     t += 1
 
-#plot_correlator(csJK[0])
-#plot_effmass(csJK[0])
-print fit_twopoint_cfuns(csJK[0]/10000., 10, 22, 64)
+plot_correlator(csJK[1])
+plot_effmass(csJK[1])
+#print fit_twopoint_cfuns(csJK[0]/10000., 10, 22, 64)
+
 
