8fbe2b48e43234f2425e00860681544c6948a7ad
Andrew Lytle <atlytle@gmail.com>
2014-03-12 12:54:44 +0530

    Towards constructing pion from two props.

--- read_overlap.py
+++ read_overlap.py
@@ -12,11 +12,15 @@ ns = 4  # N_spin.
 ndouble = V*nt*nc*nc*ns*ns*2  # Number of 8byte numbers expected.
 
 def prop_name(config, m):
-    return 'l2464f211b600m0102m0509m635a.'\
+    root = '/user5/nilmani/projects/HISQ/quark_prop/L24T64/'
+    return root + 'l2464f211b600m0102m0509m635a.'\
     '{0}.gf_hyp_prop_src_wall_t00_m{1:7f}'.format(config, m)
 
 def correlator_name(m):
     return 'OOpion_l2464__m{0:7f}_m{0:7f}.npy'.format(m)
+    
+def correlator_name2(m1, m2):
+    return 'OOpion_l2464__m{0:7f}_m{1:7f}.npy'.format(m1, m2)
 
 def extract_t(filename, t):
     '''Extract the pion correlation function at time t from the propagator.'''
@@ -28,20 +32,54 @@ def extract_t(filename, t):
             tmp.append(np.sum(np.square(data)))
     return np.sum(tmp)
     
+def extract_t4(filename, t):
+    '''Extract the pion correlation function at time t from the propagator.'''
+    # Loop structure: s c r/i s c t z y x.
+    # Pluck out bits at t. Store in tmp.
+    tmp = np.array([])
+    for i in range(2*nc*nc*ns*ns):
+        with open(filename, "rb") as f:  # Inefficient?
+            f.seek(i*8*nt*V + 8*t*V, 0)
+            data = np.fromfile(f, dtype='>d', count=V)
+            tmp = np.hstack((tmp, data))
+    # Convert to complex numbers.  Store in ctmp.
+    ctmp = np.array([])
+    for chunk in np.hsplit(tmp, ns*nc):
+        chunk_re, chunk_im = np.hsplit(chunk, 2)
+        chunk_c = map(complex, chunk_re, chunk_im)
+        ctmp = np.hstack((ctmp, chunk_c))
+        
+    return np.sum(ctmp*np.conj(ctmp))
+            
 def pion_correlator(filename):
-    '''Construct pion correlator from propagator.'''
+    "Construct pion correlator from propagator."
     correlator = np.zeros((nt))
     for t in range(nt):
         correlator[t] = extract_t(filename, t)
     return correlator
+         
+def pion_correlator3(filename):
+    "Construct pion correlator from propagator."
+    correlator = np.zeros((nt))
+    for t in range(nt):
+        correlator[t] = extract_t4(filename, t)
+    return correlator
+    
+def pion_correlator2(file1, file2):
+    "Construct pion correlator from two propagators."
+    correlator = np.zeros((nt))
+    for t in range(nt):
+        correlator[t] = extract_t2(file1, file2, t)
+    return correlator
+        
 
 def check_length(filename):
     '''Ensure the file has the expected size.'''
     if getsize(filename) != ndouble*8:
         raise Exception('{0} does not have the expected size.'.format(filename))
 
-
-def main(files):
+def convert_single_propagators(files):
+    "Construct pion correlators from individual overlap propagators."
     # Some basic checks on the input.
     head0, config0, middle0, mass0 = files[0].split('.')
     for f in files:
@@ -64,6 +102,20 @@ def main(files):
     # Write output.
     m = float('0.'+mass0)
     np.save(correlator_name(m), correlators) 
+
+def main(files):
+    
+    # Specify propagators to parse.
+    config_list = [1020, 1040]
+    flist1 = [propagator_name(config, 0.0745) for config in config_list]
+    flist2 = [propagator_name(config, 0.001) for config in config_list]
+    
+    # Construct the block of correlators.
+    correlators = []
+    for f1, f2 in zip(flist1, flist2):
+        print f1
+        print f2, '\n'
+
     
     return 0
    
