20228da4b208b569f09c67f8675e885473942697
Andrew Lytle <atlytle@gmail.com>
2014-03-20 12:41:38 +0530

    Effective mass plots.

--- HISQ_pion.py
+++ HISQ_pion.py
@@ -1,7 +1,8 @@
 import numpy as np
+import pylab as p
 
 from read_HISQ import correlator_name, correlator_name2
-from resample import JK_block
+from resample import JK_block, JKsigma
 from calc_overlap import plot_correlator #fit_twopoint_cfuns,plot_effmass
 from correlators import fit_twopoint_cfuns, fit_cfuns_double_cosh_osc, naive_effmass
 
@@ -16,6 +17,28 @@ def plot_effmass(cfnc, save=False, name='', title=None, yrange=[]):
     p.xlabel('$t$')
     p.ylabel('$|\\frac{C[t+1]}{C[t]}|$')
     p.errorbar(range(len(ave)), ave, sigma, fmt='ko')
+    if save:
+        p.savefig(name)
+    else:
+        p.show()
+        
+def plot_effmass_with_fit(cfnc, ti, tf, T, 
+                          save=False, name='', title=None, yrange=[]):
+    "Also show the mass determined from a fit."
+    effmass = naive_effmass(cfnc)
+    ave, sigma = effmass[0], JKsigma(effmass)
+    fit = fit_twopoint_cfuns(cfnc, ti, tf, T)
+    m, sigm = fit[0][1], fit[1][1]
+    p.figure()
+    if title is not None:
+        p.title(title)
+    if yrange:
+        p.ylim(yrange)
+    p.xlabel('$t$')
+    p.ylabel('$|\\frac{C[t+1]}{C[t]}|$')
+    p.errorbar(range(len(ave)), ave, sigma, fmt='ko')
+    p.hlines([m-sigm, m+sigm], ti, tf, colors='r')
+    #p.axhspan((m-sigm), (m+sigm), facecolor=(1.0,0.0,0.0), alpha=0.5) 
     if save:
         p.savefig(name)
     else:
@@ -49,6 +72,7 @@ plot_correlator(c_0509_635JK.real)
 plot_effmass(c_0509_635JK.real)
 print fit_twopoint_cfuns(c_0509_635JK.real/10000., 10, 22, 64)
 print fit_cfuns_double_cosh_osc(c_0509_635JK.real/10000., 10, 22, 64)
+plot_effmass_with_fit(c_0509_635JK.real, 10, 22, 64)
 
 
 
