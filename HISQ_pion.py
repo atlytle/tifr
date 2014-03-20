ca5ad7d7a75b112b4c0ac25dd9cf174a7216a1ba
Andrew Lytle <atlytle@gmail.com>
2014-03-20 14:33:16 +0530

    Class wrapper for pion correlators.

--- HISQ_pion.py
+++ HISQ_pion.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import pylab as p
 
@@ -43,51 +44,70 @@ def plot_effmass_with_fit(cfnc, ti, tf, T,
         p.savefig(name)
     else:
         p.show()
+        
+class pion:
+    "Wrapper for pion correlation functions."
+    def __init__(self, m1, m2):
+        self.m1, self.m2 = m1, m2
+        self.correlators = np.load(correlator_name2(m1, m2))
+        self.JKcorrelators = JK_block(self.correlators)
 
-#root = ''
-#mlist = ['635', '0102', '0509']
-#cs = [np.load(correlator_name(m)) for m in mlist]
-#csJK = map(JK_block, cs)
+def main(argv=None):
+    #root = ''
+    #mlist = ['635', '0102', '0509']
+    #cs = [np.load(correlator_name(m)) for m in mlist]
+    #csJK = map(JK_block, cs)
 
-#c_635_0509 = np.load(correlator_name2('635', '0509'))
-#c_635_0509JK = JK_block(c_635_0509)
-
-
-c_0509_635 = np.load(correlator_name2('0509', '635'))
-c_0509_635JK = JK_block(c_0509_635)
-
-c_0509_0509 = np.load(correlator_name2('0509', '0509'))
-c_0509_0509JK = JK_block(c_0509_0509)
+    #c_635_0509 = np.load(correlator_name2('635', '0509'))
+    #c_635_0509JK = JK_block(c_635_0509)
 
 
-print c_0509_0509.shape
+    c_0509_635 = np.load(correlator_name2('0509', '635'))
+    c_0509_635JK = JK_block(c_0509_635)
 
-#t = 0
-#for x in c_0509_635JK[0]:
-#    print t, x.real,' ', x.imag
-#    t += 1
+    c_0509_0509 = np.load(correlator_name2('0509', '0509'))
+    c_0509_0509JK = JK_block(c_0509_0509)
     
+    test = pion('0509', '0509')
     
-plot_correlator(c_0509_635JK.real)
-plot_effmass(c_0509_635JK.real)
-print fit_twopoint_cfuns(c_0509_635JK.real/10000., 10, 22, 64)
-print fit_cfuns_double_cosh_osc(c_0509_635JK.real/10000., 10, 22, 64)
-plot_effmass_with_fit(c_0509_635JK.real, 10, 22, 64)
+    assert (test.JKcorrelators == c_0509_0509JK).all()
+
+
+    print c_0509_0509.shape
+
+    #t = 0
+    #for x in c_0509_635JK[0]:
+    #    print t, x.real,' ', x.imag
+    #    t += 1
+        
+        
+    plot_correlator(c_0509_635JK.real)
+    plot_effmass(c_0509_635JK.real)
+    print fit_twopoint_cfuns(c_0509_635JK.real/10000., 10, 22, 64)
+    print fit_cfuns_double_cosh_osc(c_0509_635JK.real/10000., 10, 22, 64)
+    plot_effmass_with_fit(c_0509_635JK.real, 10, 22, 64)
+
+#    for ti in range(8, 15):
+#        for tf in range(18, 32):
+#            print ti, tf, ": ", fit_twopoint_cfuns(c_0509_635JK.real/10000., ti, tf, 64)
 
 
 
-#plot_correlator(c_0509_635JK.real)
-#plot_effmass(c_0509_635JK.real)
-#print fit_twopoint_cfuns(c_0509_635JK.real/10000., 10, 22, 64)
+    #plot_correlator(c_0509_635JK.real)
+    #plot_effmass(c_0509_635JK.real)
+    #print fit_twopoint_cfuns(c_0509_635JK.real/10000., 10, 22, 64)
 
-#plot_correlator(csJK[0])
-#plot_correlator(csJK[1])
-#plot_correlator(csJK[2])
+    #plot_correlator(csJK[0])
+    #plot_correlator(csJK[1])
+    #plot_correlator(csJK[2])
 
-#plot_effmass(csJK[0])
-#plot_effmass(csJK[1])
-#plot_effmass(csJK[2])
+    #plot_effmass(csJK[0])
+    #plot_effmass(csJK[1])
+    #plot_effmass(csJK[2])
 
-#print fit_twopoint_cfuns(csJK[0]/10000., 10, 22, 64)
-#print fit_twopoint_cfuns(csJK[1]/100000., 10, 22, 64)
-#print fit_twopoint_cfuns(csJK[2]/10000., 10, 22, 64)
+    #print fit_twopoint_cfuns(csJK[0]/10000., 10, 22, 64)
+    #print fit_twopoint_cfuns(csJK[1]/100000., 10, 22, 64)
+    #print fit_twopoint_cfuns(csJK[2]/10000., 10, 22, 64)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
