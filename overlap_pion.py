2991dc6b406f006838545ab4d6f11378625aedd0
Andrew Lytle <atlytle@gmail.com>
2014-03-21 16:51:42 +0530

    Wrapper for pion correlators.

--- overlap_pion.py
+++ overlap_pion.py
@@ -16,31 +16,9 @@ class pion:
         self.JKcorrelators = JK_block(self.correlators)
         
 def main(argv=None):
-    root = ''
-    mlist = [0.0745, 0.001] #0.001 has no signal
-
-    cs = [np.load(correlator_name(m)) for m in mlist]
-    csJK = map(JK_block, cs)
     
     pions = [pion(0.001, 0.001), pion(0.0745, 0.0745)]
-    
-    #print cs[0].shape
-    #print csJK[0].shape
-    t=0
-    for x in cs[0][0]:  # correlator
-        print t, x
-        t += 1
-        
-    print ''
-    
-    t=0
-    for x in pions[1].correlators[0]:  # correlator
-        print t, x
-        t += 1
 
-    #plot_correlator(csJK[1])
-    #plot_effmass(csJK[1])
-    #print fit_twopoint_cfuns(csJK[0]/10000., 10, 22, 64)
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
