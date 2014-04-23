e22fd2604f408bfa84615f811450316e620539e4
Andrew <atlytle@gmail.com>
2014-04-23 14:22:21 +0530

    General HISQ mesons.

---
+++ hisq_meson.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+from read_mixed import gx, gy, gz, gt, g5, id4
+
+nx, ny, nz, nt = 24, 24, 24, 64
+nc = 3
+ns = 4
+
+def main(filename):
+	pass
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1]))
