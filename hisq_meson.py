2b35210673ebf103f2056bd2bca36f72fe5cf0ba
Andrew <atlytle@gmail.com>
2014-04-23 15:23:34 +0530

    Kaon check.

--- hisq_meson.py
+++ hisq_meson.py
@@ -30,14 +30,38 @@ def pion_correlator(propname):
     assert (np.conjugate(tmp2) == tmp1).all()  # For pion.
     return (tmp1*tmp2).astype(np.complex128).sum()
 
+def kaon_correlator(prop1, prop2):
+    '''Pseudoscalar correlator using two Wilsonized propagators.
+
+    Mainly for illustrative purposes.
+    '''
+    
+    # First propagator.
+    tmp = extract_t_fromfile(prop1, 0)  # Extract timeslice 0.
+    tmp = reshape_HISQ(tmp)  # Shape is now (nx*ny*nz, ns, ns).
+    tmp = wilson_matrix*tmp  # Multiply in spin.
+    tmp1 = spinmult(g5, tmp)
+    tmp1 = spinmult(g5, tmp1)
+    assert (tmp == tmp1).all()  # For pion.
+    
+    # Second propagator.
+    tmp = extract_t_fromfile(prop2, 0)
+    tmp = reshape_HISQ(tmp)
+    tmp = wilson_matrix*tmp
+    tmp2 = np.conjugate(np.transpose(tmp, (0,2,1)))  # Transpose on spin.
+    tmp2 = spinmult(g5, tmp2)
+    tmp2 = spinmult(g5, tmp2)
+    tmp2 = np.transpose(tmp2, (0,2,1))
+    
+    return (tmp1*tmp2).astype(np.complex128).sum()
+
 def main():
     # Load HISQ propagators.
     prop1 = propagator_name('635', 1000)
+    prop2 = propagator_name('0509', 1000)
 
-    # Results from read_HISQ.py.
-    print pion_corr(prop1)[0]  # Agreement w/ slight precision loss? in +=?
-    print pion_correlator2(prop1,prop1)[0]  # Total agreement.
-    print pion_correlator(prop1)/4  # Total agreement.
+    print pion_correlator2(prop1, prop2)[0]  # Total agreement.
+    print kaon_correlator(prop1, prop2)/4  # Total agreement.
 
 if __name__ == "__main__":
     sys.exit(main())
