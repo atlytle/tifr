620a39fe110940e95305b8bedd3529213cac87ac
Andrew <atlytle@gmail.com>
2014-04-29 17:00:25 +0530

    Verified overlap rho mesons against Nilmani.

--- overlap_meson.py
+++ overlap_meson.py
@@ -36,24 +36,33 @@ def pion_correlator(propname):
 def meson_correlator(propname, g1, g2):
     '''General meson correlator.
 
-    Has the form Tr sum_x g1 g5 S^+ g5 g2 S.
+    Has the form Tr sum_x g2 g5 S^+ g5 g1 S.
     '''
+    corr = np.zeros((nt), dtype=complex)
     for t in range(nt):
         tmp = extract_t5(propname, t)  # Extract timeslice t.
         tmp = reshape_overlap(tmp)  # Shape is now (nx*ny*nz, 4, 4).
-        tmp1 = spinmult(g5, tmp)
-        tmp1 = spinmult(g1, tmp1)
+        tmp1 = spinmult(g1, tmp)
+        tmp1 = spinmult(g5, tmp1)
         tmp2 = np.conjugate(np.transpose(tmp, (0,2,1)))  # Transpose on spin.
         tmp2 = spinmult(g5, tmp2)
         tmp2 = spinmult(g2, tmp2)
         tmp2 = np.transpose(tmp2, (0,2,1))
-        print t, np.sum(tmp1*tmp2)
+        corr[t] = np.sum(tmp1*tmp2)
+    return corr
 
 
 
 def main(filename):
-    print pion_correlator(filename)
-    meson_correlator(filename, gx, gx)
+    #print pion_correlator(filename)
+    rho_x = meson_correlator(filename, gx, gx)
+    rho_y = meson_correlator(filename, gy, gy)
+    rho_z = meson_correlator(filename, gz, gz)
+    rho = rho_x + rho_y + rho_z
+    t=0
+    for c in rho:
+        print t, c
+        t += 1
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1]))
